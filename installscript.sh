f308ec78d86a07d89763ab58a28e53d4bde900dc
Basil Paul <basil.paul@digitaslbi.com>
2018-05-31 15:59:51 +0100

    Updating install script

--- installscript.sh
+++ installscript.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo "Setting up postgres cluster.."
+echo
 eval $(minikube docker-env)
 kubectl apply -f pvc.yaml
 kubectl apply -f secrets.yaml
 kubectl apply -f postgres-deployment.yaml
 kubectl apply -f postgres-service.yaml
+echo "Building docker image of app.."
+echo
 docker build -t myrest:1.0 .
 kubectl apply -f rest-deployment.yaml
 kubectl apply -f rest-service.yaml
